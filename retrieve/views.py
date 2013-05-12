f7ae31e517bd0a09c5ddc41d856a1c8a6424475a
Joseph Redfern <joseph@redfern.me>
2013-05-12 17:24:35 +0100

    Limit to 1000 data points

--- retrieve/views.py
+++ retrieve/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from retrieve.models import DataPoint
 import time
+import math
 
 
 def graph(request):
@@ -10,9 +11,21 @@ def graph(request):
     data = DataPoint.objects.all()
     dataset = []
 
+    max_points = 1000
+    if(len(dataset)>max_points):
+        spacing = int(math.floor(len(dataset)/max_points))
+    else:
+        spacing = 1
+
+    point_counter = 0
     for point in data:
-        int(time.mktime(point.time.timetuple())*1000)
-        dataset.append({'time':int(time.mktime(point.time.timetuple())*1000)-int(time.mktime(data[0].time.timetuple())*1000), 'value':point.value-data[0].value})
+        point_counter += 1
+
+        if(point_counter%spacing==0):
+            dataset.append({
+                'time':int(time.mktime(point.time.timetuple()))-int(time.mktime(data[0].time.timetuple())),
+                'value':point.value-data[0].value,
+                })
 
     values['data'] = dataset
 
