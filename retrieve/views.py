932a2c07de31bc691732fb37bf803fd6cd4b226b
Joseph Redfern <joseph@redfern.me>
2013-05-12 22:56:18 +0100

    more efficient dealings

--- retrieve/views.py
+++ retrieve/views.py
@@ -14,9 +14,9 @@ def graph(request):
     max_points = 1000
     values['yoffset'] = data[0].value
 
-    if(len(data) > max_points):
-        spacing = int(math.floor(len(data) / max_points))
-        if spacing<1:
+    if(DataPoint.objects.count() > max_points):
+        spacing = int(math.floor(float(DataPoint.objects.count()) / max_points))
+        if spacing < 1:
             spacing = 1
     else:
         spacing = 1
@@ -40,13 +40,17 @@ def graph(request):
 
     values['mine_rate'] = dict()
 
-    if(len(mostRecentFirst)>1):
-        values['mine_rate']['10s'] = float((mostRecentFirst[0].value-mostRecentFirst[1].value))/10
-    if(len(mostRecentFirst)>6): 
-        values['mine_rate']['1m'] = float((mostRecentFirst[0].value-mostRecentFirst[6].value))/60
-    if(len(mostRecentFirst)>30):
-        values['mine_rate']['5m'] = float((mostRecentFirst[0].value-mostRecentFirst[30].value))/300
-    if(len(mostRecentFirst)>90):
-        values['mine_rate']['15m'] = float((mostRecentFirst[0].value-mostRecentFirst[90].value))/900
+    if(len(mostRecentFirst) > 1):
+        values['mine_rate']['10s'] = float((mostRecentFirst[
+                                           0].value - mostRecentFirst[1].value)) / 10
+    if(len(mostRecentFirst) > 6):
+        values['mine_rate']['1m'] = float((mostRecentFirst[
+                                          0].value - mostRecentFirst[6].value)) / 60
+    if(len(mostRecentFirst) > 30):
+        values['mine_rate']['5m'] = float((mostRecentFirst[
+                                          0].value - mostRecentFirst[30].value)) / 300
+    if(len(mostRecentFirst) > 90):
+        values['mine_rate']['15m'] = float((mostRecentFirst[
+                                           0].value - mostRecentFirst[90].value)) / 900
 
     return render(request, "graph.html", values)
