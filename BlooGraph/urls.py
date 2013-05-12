0c874fa09545fc684b41627f1e1ca6a13929f3c0
Joseph Redfern <joseph@redfern.me>
2013-05-12 17:09:42 +0100

    Added graph

--- BlooGraph/urls.py
+++ BlooGraph/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'BlooGraph.views.home', name='home'),
+    url(r'^$', 'retrieve.views.graph', name='graph'),
     # url(r'^BlooGraph/', include('BlooGraph.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- retrieve/views.py
+++ retrieve/views.py
@@ -1,1 +1,20 @@
 # Create your views here.
+from django.shortcuts import render
+from retrieve.models import DataPoint
+import time
+
+
+def graph(request):
+    values = dict()
+
+    data = DataPoint.objects.all()
+    dataset = []
+
+    for point in data:
+        int(time.mktime(point.time.timetuple())*1000)
+        dataset.append({'time':int(time.mktime(point.time.timetuple())*1000)-int(time.mktime(data[0].time.timetuple())*1000), 'value':point.value-data[0].value})
+
+    values['data'] = dataset
+
+
+    return render(request, "graph.html", values)
